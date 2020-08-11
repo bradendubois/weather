42905f72fa44171cef91db9a2bbbf7658197dac5
Braden <drab3n@gmail.com>
2020-08-11 14:35:04 -0600

    Added conditional "gust" check as not always in response

--- parse_response.py
+++ parse_response.py
@@ -12,10 +12,12 @@ def parse_response(api_response: dict):
         "maximum": temperature_conversion(main["temp_max"]),
         "humidity": main["humidity"],
         "wind_speed": wind_speed_conversion(wind["speed"]),
-        "gusts": wind_speed_conversion(wind["gust"]),
         "direction": wind_direction_conversion(wind["deg"]),
         "name": api_response["name"],
         "description": api_response["weather"][0]["description"]
     }
+
+    if "gust" in wind: 
+        response["gusts"] = wind_speed_conversion(wind["gust"])
 
     return response
--- print_response.py
+++ print_response.py
@@ -6,12 +6,14 @@ BOTTOM_PADDING = 0
 
 def print_response(parsed_response: dict):
 
+    gust = ", gusts of {:.1f}{}".format(parsed_response["gusts"], wind_symbol) if "gusts" in parsed_response else ""
+
     message = [
         "Currently, in {}:".format(parsed_response["name"]),
         "{}".format(parsed_response["description"][0].upper() + parsed_response["description"][1:]),
         "Temperature: {:.1f}°{} (feels like: {:.1f}°{})".format(parsed_response["temperature"], temperature_symbol, parsed_response["feels_like"], temperature_symbol),
         "High: {:.0f}°{}, Low: {:.0f}°{}".format(parsed_response["maximum"], temperature_symbol, parsed_response["minimum"], temperature_symbol),
-        "Wind ({}) speeds of {:.1f}{}, gusts of {:.1f}{}".format(parsed_response["direction"], parsed_response["wind_speed"], wind_symbol, parsed_response["gusts"], wind_symbol),
+        "Wind ({}) speeds of {:.1f}{}{}".format(parsed_response["direction"], parsed_response["wind_speed"], wind_symbol, gust),
         "Humidity: {:.0f}%".format(parsed_response["humidity"])
     ]
 
